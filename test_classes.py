256f5a40ebc9035bdf0f36d9780893f9e753a466
camhanson <camhanson@comcast.net>
2019-09-23 14:28:08 -0400

    Testing class creation

---
+++ test_classes.py
@@ -0,0 +1,16 @@
+from classes_ex import *
+
+v1 = Vehicle('A', (0,0), (1,2))
+v2= Vehicle ('B', (5,10),(2,-1))
+
+#v1.print_info()
+#v2.print_info()
+
+c1 = Car('C',(3,4),(5,6),15, 20, 50000)
+c1.print_info()
+c1.go(45)
+c1.print_info()
+
+
+
+
